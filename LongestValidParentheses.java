8ced453f32d0c93d82544324c2db923b032ad064
Mayank Kumar <159799813+Mayank-888@users.noreply.github.com>
2026-07-23 19:46:59 +0530

    Update LongestValidParentheses.java

--- LongestValidParentheses.java
+++ LongestValidParentheses.java
@@ -1,92 +1,52 @@
-import java.util.*;
+class AlgorithmMain {
+	int getLongestValidSubstring(String input) {
+		int[] arr = new int[input.length()];
+		int open = 0;
+		for (int i=0; i<input.length(); i++) {
+			char current = input.charAt(i);
+			if (current == '(') {
+				open++;
+				arr[i] = 0;
+			} else {
+				if (open > 0) {
+					open--;
+					if (i - 1 >= 0) {
+						arr[i] = arr[i-1] + 2;
+						if (i - arr[i] >= 0) {
+							arr[i] = arr[i] + arr[i-arr[i]];
+						} else {
+							System.out.println("There is no such element");
+						}
+					} else {
+						System.out.println("There is no prev element");
+					}
+				} else {
+					arr[i] = 0;
+					continue;
+				}
+			}
+		}
+		return getMaxValue(arr);
+	}
+	
+	int getMaxValue (int[] arr) {
+		if(arr.length == 0) return 0;
+		int max = arr[0];
+		for (int i = 1; i<arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+}
 
-public class LongestValidParentheses {
-
-    public static int longestValidParentheses(String s) {
-
-        char[] arr = s.toCharArray();
-
-        int left = 0;
-        int right = 0;
-        int max = 0;
-
-        // Left to Right 
-        for (int i = 0; i < arr.length; i++) {
-
-           
-            if (arr[i] == '(') {
-                left++;
-            }
-
-           
-            else {
-                right++;
-            }
-
-           
-            if (left == right) {
-
-                int length = 2 * left;
-
-                if (length > max) {
-                    max = length;
-                }
-            }
-
-         
-            else if (right > left) {
-
-                left = 0;
-                right = 0;
-            }
-        }
-
-        // Right to Left 
-        left = 0;
-        right = 0;
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-
-            if (arr[i] == '(') {
-                left++;
-            }
-
-            else {
-                right++;
-            }
-
-           
-            if (left == right) {
-
-                int length = 2 * left;
-
-                if (length > max) {
-                    max = length;
-                }
-            }
-
-           
-            else if (left > right) {
-
-                left = 0;
-                right = 0;
-            }
-        }
-
-        return max;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter Parentheses String: ");
-        String s = sc.nextLine();
-
-        int ans = longestValidParentheses(s);
-
-        System.out.println("Longest Valid Parentheses Length = " + ans);
-
-        sc.close();
-    }
+public class Algorithm2 {
+	public static void main(String[] args) {
+		System.out.println("Started main");
+		AlgorithmMain algo = new AlgorithmMain();
+		int result = algo.getLongestValidSubstring(")()()))()(())()()(");
+		System.out.println(result);
+	}
+	
 }
